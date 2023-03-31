92821e35c95ab1bc5a2f5e3ab91ebb77f0275d05
Myint Myat <solidifyarmor@gmail.com>
2023-03-31 10:42:18 +0630

    rename dir

--- downloader_worker.js
+++ downloader_worker.js
@@ -10,19 +10,19 @@ export default async function downloadVideos(videoUrls) {
 const { id, resolution, videoUrls } = workerData
 
 // const downloadFile = fs.createWriteStream(`./temp/${id}[${resolution}].txt`, { flags: 'a' })
-const dirname = `${id}[${resolution}]`
+const dirname = `${id}-${resolution}`
 
-await fs.promises.mkdir(dirname, { recursive: true, mode: 0o777 })
-console.log(dirname)
+fs.mkdirSync(dirname)
 
 const downloadChunksCmd = `wget -i $'${videoUrls.join('\n')}' -P ${dirname}/ -t 3 --limit-rate=10M`
-const mergeChunksCmd = `ffmpeg -i "concat:$(ls -v ${dirname}/*.ts | tr '\n' '|')" -vf "drawtext=text='PYONERIP.COM':x=10:y=h-th-10:fontcolor=white@0.5:fontsize=12:alpha=0.5" -c:v libx264 -preset medium -crf 23 -c:a aac -b:a 128k ${id}[${resolution}].mp4`
+const mergeChunksCmd = `ffmpeg -i "concat:$(ls -v ${dirname}/*.ts | tr '\n' '|')" -vf "drawtext=text='PYONERIP.COM':x=10:y=h-th-10:fontcolor=white@0.5:fontsize=12:alpha=0.5" -c:v libx264 -preset medium -crf 23 -c:a aac -b:a 128k ${id}-${resolution}.mp4`
 
 const cp = exec(downloadChunksCmd)
 
 console.log('inside downloader worker')
 
 cp.on('exit', (code) => {
+    if (code != 0) throw new Error(code)
     // parentPort.postMessage(`downloader exited with code ${code}`)
     exec(mergeChunksCmd, (err, stdout, stderr) => {
         if (err) {
@@ -30,7 +30,10 @@ cp.on('exit', (code) => {
         }
         console.log(stdout)
         console.log(parentPort.postMessage('done'))
-    }).on('exit', (code) => parentPort.postMessage(`merger exited with code ${code}`))
+    }).on('exit', (code) => {
+        if (code == 0)
+            parentPort.postMessage(`merger exited with code ${code}`)
+    })
 })
 
 // videoUrls.urls.forEach((url) => downloadFile.write(`${url}\n`))
