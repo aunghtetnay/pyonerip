120a9560d73e4483f883b5ae1c36c14d91bd8b4d
AMMO <solidifyarmor@gmail.com>
2023-04-01 11:39:14 +0630

    modularize downloader

--- saveChunks.js
+++ saveChunks.js
@@ -4,40 +4,41 @@ import { createWriteStream } from 'fs'
 import { mkdir, writeFile } from 'fs/promises'
 
 export default async function saveChunks(filename, chunksUrlList) {
-    // create directory
+    // temp directory is the same as the filename
     const dirname = filename
+    // create directory
     await mkdir(dirname)
 
+    const urlFileName = await writeUrlFile(dirname, chunksUrlList)
+
+    // run download command
+    return new Promise((resolve, reject) => {
+        // wget: read urls from file, save to directory, retry 3 times, limit rate to 10M
+        // download chunks' name is the last part of url (wget feature)
+        // bind child process's stdio to parent process's stdio to show progress
+        const cp = spawn('wget', ['-i', `${dirname}/${urlFileName}`, '-P', `${dirname}/`, '-t', '3', '--limit-rate=10M'], { stdio: 'inherit' })
+        cp.on('spawn', () => console.log('spawned', process.pid))
+        cp.on('exit', (code) => code != 0 ? reject(code) : resolve(dirname))
+    })
+}
+
+async function writeUrlFile(dirname, chunksUrlList) {
     // write chunks url list to file
+    const filename = 'urls.txt'
+
+    // use streams to write file
     const readStream = Readable.from(chunksUrlList)
-    const writeStream = createWriteStream(`${dirname}/urls.txt`)
+    const writeStream = createWriteStream(`${dirname}/${filename}`)
     const transformStream = new Transform({
         transform(chunk, encoding, callback) {
             this.push(`${chunk}\n`)
             callback()
         }
     })
-    console.time('writing file')
-    // 0.998ms
-    // writeFile(`${dirname}/urls.txt`, chunksUrlList.join('\n'))
-    // 0.631ms
-    readStream.pipe(transformStream).pipe(writeStream)
-    console.timeEnd('writing file')
 
-    // construct download command
-    const downloadChunksCmd = `wget -i ${dirname}/urls.txt -P ${dirname}/ -t 3 --limit-rate=10M`
-
-    // run download command
     return new Promise((resolve, reject) => {
-        const cp = exec(downloadChunksCmd, (err, stdout, stderr) => {
-            if (err) reject(err)
-            if (stderr) console.error(stderr)
-            if (stdout) console.log(stdout)
-        })
-        // const cp = spawn('wget', ['-i', `${dirname}/urls.txt`, '-P', `${dirname}/`, '-t', '3', '--limit-rate=10M'])
-        cp.on('spawn', () => console.log('spawned', process.pid))
-        cp.on('exit', (code) => code != 0 ? reject(code) : resolve(dirname))
-        cp.stdout.pipe(process.stdout)
-        cp.stderr.pipe(process.stderr)
+        const writeFile = readStream.pipe(transformStream).pipe(writeStream)
+        writeFile.on('finish', () => resolve(filename))
+        writeFile.on('error', reject)
     })
 }
