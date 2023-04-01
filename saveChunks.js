ff11916d73eb195d23a2a8eb2301083afeb404b8
AMMO <solidifyarmor@gmail.com>
2023-04-01 12:48:18 +0630

    coucurrent downloading

--- saveChunks.js
+++ saveChunks.js
@@ -1,7 +1,11 @@
-import { exec, spawn } from 'child_process'
+import { spawn } from 'child_process'
 import { Readable, Transform } from 'stream'
 import { createWriteStream } from 'fs'
-import { mkdir, writeFile } from 'fs/promises'
+import { mkdir } from 'fs/promises'
+
+const CHUNK_DOWNLOAD_CONCURRENCY = 50; // Limit the number of concurrent chunk downloads
+const CHUNK_DOWNLOAD_RETRY_LIMIT = 3; // Limit the number of download retries
+const CHUNK_DOWNLOAD_LIMIT_RATE = '10M'; // Limit the download rate per chunk
 
 export default async function saveChunks(filename, chunksUrlList) {
     // temp directory is the same as the filename
@@ -9,26 +13,42 @@ export default async function saveChunks(filename, chunksUrlList) {
     // create directory
     await mkdir(dirname)
 
-    const urlFileName = await writeUrlFile(dirname, chunksUrlList)
+    // Download chunks in parallel
+    const chunkDownloadPromises = []
+    for (let i = 0; i < chunksUrlList.length; i += CHUNK_DOWNLOAD_CONCURRENCY) {
+        const urlList = chunksUrlList.slice(i, i + CHUNK_DOWNLOAD_CONCURRENCY)
+        const chunkDownloadPromise = downloadChunks(urlList, dirname)
+        chunkDownloadPromises.push(chunkDownloadPromise)
+    }
+    await Promise.all(chunkDownloadPromises)
+    return dirname
+}
 
+async function downloadChunks(urlList, dirname) {
+    // create chunk url files with unique name
+    // important because we paralles chunk downloads
+    const start = urlList[0].split('/').pop()
+    const end = urlList[urlList.length - 1].split('/').pop()
+    const uniqueUrlFileName = `${start}-${end}.txt`
+    // creating url list file need to know unique name too
+    const urlFileName = await writeUrlFile(dirname, urlList, uniqueUrlFileName)
+
+    const wgetOptions = ['-i', `${dirname}/${urlFileName}`, '-P', `${dirname}/`, '-t', `${CHUNK_DOWNLOAD_RETRY_LIMIT}`, `--limit-rate=${CHUNK_DOWNLOAD_LIMIT_RATE}`]
     // run download command
     return new Promise((resolve, reject) => {
-        // wget: read urls from file, save to directory, retry 3 times, limit rate to 10M
         // download chunks' name is the last part of url (wget feature)
         // bind child process's stdio to parent process's stdio to show progress
-        const cp = spawn('wget', ['-i', `${dirname}/${urlFileName}`, '-P', `${dirname}/`, '-t', '3', '--limit-rate=10M'], { stdio: 'inherit' })
+        const cp = spawn('wget', wgetOptions, { stdio: 'inherit' })
         cp.on('spawn', () => console.log('spawned', process.pid))
         cp.on('exit', (code) => code != 0 ? reject(code) : resolve(dirname))
     })
 }
 
-async function writeUrlFile(dirname, chunksUrlList) {
-    // write chunks url list to file
-    const filename = 'urls.txt'
+async function writeUrlFile(dirname, chunksUrlList, urlFileName) {
 
     // use streams to write file
     const readStream = Readable.from(chunksUrlList)
-    const writeStream = createWriteStream(`${dirname}/${filename}`)
+    const writeStream = createWriteStream(`${dirname}/${urlFileName}`)
     const transformStream = new Transform({
         transform(chunk, encoding, callback) {
             this.push(`${chunk}\n`)
@@ -38,7 +58,7 @@ async function writeUrlFile(dirname, chunksUrlList) {
 
     return new Promise((resolve, reject) => {
         const writeFile = readStream.pipe(transformStream).pipe(writeStream)
-        writeFile.on('finish', () => resolve(filename))
+        writeFile.on('finish', () => resolve(urlFileName))
         writeFile.on('error', reject)
     })
 }
