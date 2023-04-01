ed26f0df5b9470e7f01d18abbafc422adf61773d
AMMO <solidifyarmor@gmail.com>
2023-04-01 11:07:29 +0630

    use streams instead

--- saveChunks.js
+++ saveChunks.js
@@ -1,4 +1,6 @@
-import { exec } from 'child_process'
+import { exec, spawn } from 'child_process'
+import { Readable, Transform } from 'stream'
+import { createWriteStream } from 'fs'
 import { mkdir, writeFile } from 'fs/promises'
 
 export default async function saveChunks(filename, chunksUrlList) {
@@ -7,7 +9,20 @@ export default async function saveChunks(filename, chunksUrlList) {
     await mkdir(dirname)
 
     // write chunks url list to file
-    await writeFile(`${dirname}/urls.txt`, chunksUrlList.join('\n'))
+    const readStream = Readable.from(chunksUrlList)
+    const writeStream = createWriteStream(`${dirname}/urls.txt`)
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            this.push(`${chunk}\n`)
+            callback()
+        }
+    })
+    console.time('writing file')
+    // 0.998ms
+    // writeFile(`${dirname}/urls.txt`, chunksUrlList.join('\n'))
+    // 0.631ms
+    readStream.pipe(transformStream).pipe(writeStream)
+    console.timeEnd('writing file')
 
     // construct download command
     const downloadChunksCmd = `wget -i ${dirname}/urls.txt -P ${dirname}/ -t 3 --limit-rate=10M`
@@ -19,7 +34,10 @@ export default async function saveChunks(filename, chunksUrlList) {
             if (stderr) console.error(stderr)
             if (stdout) console.log(stdout)
         })
+        // const cp = spawn('wget', ['-i', `${dirname}/urls.txt`, '-P', `${dirname}/`, '-t', '3', '--limit-rate=10M'])
         cp.on('spawn', () => console.log('spawned', process.pid))
         cp.on('exit', (code) => code != 0 ? reject(code) : resolve(dirname))
+        cp.stdout.pipe(process.stdout)
+        cp.stderr.pipe(process.stderr)
     })
 }
