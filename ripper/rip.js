9707aa410fab10a0dee661735a50d35fc75db4c7
AMMO <solidifyarmor@gmail.com>
2023-04-10 20:09:54 +0630

    delete comment

--- ripper/rip.js
+++ ripper/rip.js
@@ -18,15 +18,3 @@ export async function availableResolutions(m3u8Url) {
 export async function getVideoChunkUrls(m3u8VideoUrl) {
     return await parseVideoFromUrl(m3u8VideoUrl)
 }
-/**
-{
-    resolution: '352x240',
-    baseUrl: 'https://vz-30d21f49-6b1.b-cdn.net/bcdn_token=RJwLlH9ybyXo5ybB4NRCJbu80gyj7CQjMpjWoUn50EI&expires=1680439078&token_path=%2Fab6f2e96-83b1-4e01-ad02-32fb23f71ddd%2F/ab6f2e96-83b1-4e01-ad02-32fb23f71ddd/352x240',
-    totalDuration: 1956.7547999999813,
-    chunksUrlList: [
-      'video0.ts',  'video1.ts',  'video2.ts',  'video3.ts',  'video4.ts',
-      ... 389 more items
-    ]
-  },
-}
-*/
