f837f99b7e1a7fb97557b051466a1125f3e6ceff
AMMO <solidifyarmor@gmail.com>
2023-04-10 20:12:04 +0630

    Revert "delete comment"
    This reverts commit 9f4496b71eaf796c36429fddf60cf0bdd51e5667.

--- ripper/rip.js
+++ ripper/rip.js
@@ -14,6 +14,19 @@ export default async function rip({ name, url }) {
 export async function availableResolutions(m3u8Url) {
     return await parsePlaylistFromUrl(m3u8Url)
 }
+/**
+{
+  id: 'ab6f2e96-83b1-4e01-ad02-32fb23f71ddd',
+  baseUrl: 'https://vz-3b1-4e01-ad02-32fb23f71ddd',
+  resolutionM3U8FileUrlMap: {
+    '352x240': 'https://vz-3f71ddd/352x240/video.m3u8',
+    '640x360': 'https://vz-3f71ddd/640x360/video.m3u8',
+    '842x480': 'https://vz-3f71ddd/842x480/video.m3u8',
+    '1280x720': 'https://vz-371ddd/1280x720/video.m3u8',
+    '1920x1080': 'https://vz-371ddd/1920x1080/video.m3u8'
+  }
+}
+*/
 
 export async function getVideoChunkUrls(m3u8VideoUrl) {
     return await parseVideoFromUrl(m3u8VideoUrl)
