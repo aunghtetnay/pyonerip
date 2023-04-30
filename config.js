c2d5294c535fca70d366a722f9c7ab0de7a3f06f
AMMO <solidifyarmor@gmail.com>
2023-04-30 08:37:42 +0630

    init s3 integration

--- config.js
+++ config.js
@@ -5,3 +5,10 @@ config()
 export const PP_API_URL = 'https://app.pyoneplay.com/api/v1'
 
 export const PORT = process.env.PORT || 4000
+
+// S3
+export const SS3_URL = process.env.SS3_URL
+export const S3_REGION = process.env.S3_REGION
+export const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY
+export const S3_SECRET_KEY = process.env.S3_SECRET_KEY
+
