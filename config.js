d2d8a41cb117e7d8d119ebf9769701e9fa76cbca
AMMO <solidifyarmor@gmail.com>
2023-05-06 15:59:26 +0630

    env and config file

--- config.js
+++ config.js
@@ -11,4 +11,4 @@ export const SS3_URL = process.env.SS3_URL
 export const S3_REGION = process.env.S3_REGION
 export const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY
 export const S3_SECRET_KEY = process.env.S3_SECRET_KEY
-
+export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME
