798a1d5ea7c2a2607f5922987dec9e58e8e63675
AMMO <solidifyarmor@gmail.com>
2023-05-10 23:46:43 +0630

    additional fields

--- validators/rip.validator.js
+++ validators/rip.validator.js
@@ -1,7 +1,7 @@
 import { object, string } from 'yup'
 
 const parseSchema = object({
-    streamingUrl: string().matches(/playlist.m3u8$/, { message: 'invalid streamingUrl' }).required()
+    streaming_url: string().matches(/playlist.m3u8$/, { message: 'invalid streamingUrl' }).required()
 })
 
 export async function validateParse(req, res, next) {
@@ -13,16 +13,17 @@ export async function validateParse(req, res, next) {
     }
 }
 
-const filenameRegex = /^[a-zA-Z0-9\-]+$/
-const filenameErrorMessage = 'Invalid filename. Only alphanumeric characters and dashes are allowed.'
+const filenameRegex = /^[a-zA-Z0-9\-_]+$/
+const filenameErrorMessage = 'Invalid filename. Only alphanumeric characters, dashes and underscores are allowed.'
 const urlRegex = /video.m3u8$/
 const urlErrorMessage = 'Invalid url. Not ending with video.m3u8'
 const ripSchema = object({
     // IMPORTANT:
     // sanitize name, so that dangerous unix commands can't be executed
     name: string().matches(filenameRegex, { message: filenameErrorMessage }).required(),
+    series_name: string().matches(filenameRegex, { message: filenameErrorMessage }).required(),
     // validate correct url
-    url: string().matches(urlRegex, { message: urlErrorMessage }).required()
+    streaming_url: string().matches(urlRegex, { message: urlErrorMessage }).required()
 })
 
 export async function validateRip(req, res, next) {
