efcbb3b73fb73630fd4426b665702b6c237f2586
AMMO <solidifyarmor@gmail.com>
2023-05-10 23:45:48 +0630

    upload s3 with folder path

--- controllers/rip.controller.js
+++ controllers/rip.controller.js
@@ -3,8 +3,8 @@ import { upload } from '../helpers/uploadToS3.js'
 
 export async function parseStreamingUrl(req, res) {
     try {
-        const { streamingUrl } = req.body
-        const result = await availableResolutions(streamingUrl)
+        const { streaming_url } = req.body
+        const result = await availableResolutions(streaming_url)
         res.status(200).json(result)
     } catch (error) {
         console.log(error)
@@ -14,12 +14,13 @@ export async function parseStreamingUrl(req, res) {
 
 export async function rips(req, res) {
     try {
-        const { url, name } = req.body
+        const { streaming_url, series_name, name } = req.body
         // process, might want to dispatch to worker
-        console.log(`processing: ${url}, ${name}`)
-        const ripped = await rip({ name, url })
-        // res.status(200).json(result)
-        const uploaded = await upload(ripped, name)
+        console.log(`processing: ${streaming_url}, ${name}`)
+        const resultPath = await rip({ name, url: streaming_url })
+        // strip the path to get the filename
+        const resultFileName = resultPath.slice(resultPath.lastIndexOf('/') + 1)
+        const uploaded = await upload(resultPath, `${series_name}/${resultFileName}`)
         res.status(200).json(uploaded)
 
     } catch (error) {
